1a2e289ec71bcd9f428b7c6012c3a54b91dcd313
Basile Dura <basile.dura-ext@aphp.fr>
2022-05-11 16:50:59 +0200

    feat: remove outdated registry entry

--- edspdf/reg.py
+++ edspdf/reg.py
@@ -6,7 +6,6 @@ import catalogue
 class Registry:
 
     extractors = catalogue.create("edspdf", "extractors", entry_points=True)
-    params = catalogue.create("edspdf", "params", entry_points=True)
     readers = catalogue.create("edspdf", "readers", entry_points=True)
     aggregators = catalogue.create("edspdf", "aggregators", entry_points=True)
     transforms = catalogue.create("edspdf", "transforms", entry_points=True)
@@ -15,7 +14,6 @@ class Registry:
     _catalogue = dict(
         extractors=extractors,
         aggregators=aggregators,
-        params=params,
         readers=readers,
         classifiers=classifiers,
         transforms=transforms,
