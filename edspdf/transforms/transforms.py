62192cceaeaaaecb9320839d31211809baf42602
Basile Dura <basile.dura-ext@aphp.fr>
2022-06-02 10:16:27 +0200

    fix: keep origin on top-left corner with rescale
    name columns differently

--- edspdf/transforms/transforms.py
+++ edspdf/transforms/transforms.py
@@ -53,10 +53,10 @@ class AddDimensions(BaseTransform):
 class Rescale(BaseTransform):
     def transform(self, df: pd.DataFrame) -> pd.DataFrame:
 
-        df.x0 = df.x0 * df.page_width
-        df.x1 = df.x1 * df.page_width
+        df["x0_"] = df.x0 * df.page_width
+        df["x1_"] = df.x1 * df.page_width
 
-        df.y0 = (1 - df.y0) * df.page_height
-        df.y1 = (1 - df.y1) * df.page_height
+        df["y0_"] = df.y0 * df.page_height
+        df["y1_"] = df.y1 * df.page_height
 
         return df
