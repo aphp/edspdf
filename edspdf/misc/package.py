b5ba8e2ddcf7a9e26ec7d3d242d53ffb064f420b
Basile Dura <basile.dura-ext@aphp.fr>
2022-07-06 15:19:16 +0200

    feat: add existence test for package resource

--- edspdf/misc/package.py
+++ edspdf/misc/package.py
@@ -11,4 +11,7 @@ def package_resource(path: Path, package: Optional[Path] = None) -> Path:
     with resources.path(package=package, resource="__init__.py") as p:
         path = p.parent / path
 
+    if not path.exists():
+        raise FileNotFoundError(f"{path} does not exist.")
+
     return path
--- tests/misc/test_package_resources.py
+++ tests/misc/test_package_resources.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+import pytest
+
 from edspdf.misc import package_resource
 
 BASE_DIR = Path(__file__).parent.parent.parent
@@ -8,3 +10,6 @@ BASE_DIR = Path(__file__).parent.parent.parent
 def test_package_resource():
     path = package_resource("reg.py", "edspdf")
     assert path == BASE_DIR / "edspdf" / "reg.py"
+
+    with pytest.raises(FileNotFoundError):
+        package_resource("dummy.py", "edspdf")
