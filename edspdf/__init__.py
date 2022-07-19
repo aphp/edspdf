25f8c6b439e5215d4fbf77de05eeafcd85cf21bc
Basile Dura <basile@bdura.me>
2022-07-19 12:09:23 +0200

    fix: make sure registry is imported first

--- edspdf/__init__.py
+++ edspdf/__init__.py
@@ -1,5 +1,6 @@
-from edspdf import aggregation, classification, extraction, misc, reading, transforms
-from edspdf.loading import from_str, load
 from edspdf.reg import registry
+
+from . import aggregation, classification, extraction, misc, reading, transforms
+from .loading import from_str, load
 
 __version__ = "0.4.2"
