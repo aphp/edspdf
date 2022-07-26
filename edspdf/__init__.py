c1e4c150b88acea2e3d348b3f5d9d8acf836598a
Basile Dura <basile@bdura.me>
2022-07-26 10:56:10 +0200

    chore: bump version

--- edspdf/__init__.py
+++ edspdf/__init__.py
@@ -5,4 +5,4 @@ from edspdf.reg import registry
 from . import aggregators, classifiers, extractors, misc, readers, transforms
 from .loading import from_str, load
 
-__version__ = "0.5.0"
+__version__ = "0.5.1"
