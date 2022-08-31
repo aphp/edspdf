7422641ffb33c3fb661c1b01624ca2d6f2ee6c83
Perceval Wajsburt <perceval.wajsburt@sorbonne-universite.fr>
2022-08-31 10:25:17 +0200

    chore: bump version to 0.5.3

--- edspdf/__init__.py
+++ edspdf/__init__.py
@@ -5,4 +5,4 @@ from edspdf.reg import registry
 from . import aggregators, classifiers, extractors, misc, readers, transforms
 from .loading import from_str, load
 
-__version__ = "0.5.2"
+__version__ = "0.5.3"
