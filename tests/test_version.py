7c9fd6ef68b98d50f494e58c0ed381c746542aee
Perceval Wajsburt <perceval.wajsburt@sorbonne-universite.fr>
2022-08-31 09:58:47 +0200

    tests: fix version test

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,14 +1,11 @@
 import re
 from pathlib import Path
+from edspdf import __version__ as init_version
 
 
 def test_versions():
-    init = Path("edspdf/__init__.py").read_text()
-    poetry = Path("pyproject.toml").read_text()
+    poetry = (Path(__file__).parent.parent / "pyproject.toml").read_text()
 
-    init_version = re.search(r'__version__ = "(?P<version>.+)"', init).groupdict()[
-        "version"
-    ]
     poetry_version = re.search(r'version = "(?P<version>.+)"', poetry).groupdict()[
         "version"
     ]
