219a4627f559db32ed98387be8849e2480fbc7e1
Perceval Wajsbürt <perceval.wajsburt-ext@aphp.fr>
2023-01-24 16:28:24 +0000

    feat: small api refacto, allow cfg reversing

--- edspdf/component.py
+++ edspdf/component.py
@@ -18,7 +18,6 @@ from typing import (
 
 import torch
 
-from .registry import registry
 from .utils.collections import batch_compress_dict, batchify, decompress_dict
 
 BatchT = TypeVar("BatchT")
@@ -78,46 +77,23 @@ def cached_collate(fn):
 
 def cached_forward(fn):
     @wraps(fn)
-    def wrapped(self: "Module", batch: Dict, *args, **kwargs):
-        cache_id = hash_batch(batch)
+    def wrapped(self: "Module", *args, **kwargs):
+        # Convert args and kwargs to a dictionary matching fn signature
+        call_kwargs = inspect.getcallargs(fn, self, *args, **kwargs)
+        call_kwargs.pop("self")
+        cache_id = hash_batch(call_kwargs)
         if not self._do_cache or cache_id is None:
-            return fn(self, batch, *args, **kwargs)
+            return fn(self, *args, **kwargs)
         if cache_id in self._forward_cache:
             return self._forward_cache[cache_id]
-        res = fn(self, batch, *args, **kwargs)
+        res = fn(self, *args, **kwargs)
         self._forward_cache[cache_id] = res
         return res
 
     return wrapped
 
 
-def save_init_kwargs(fn: Callable) -> Callable:
-    @wraps(fn)
-    def new_init(self, *args, **kwargs):
-        sig = inspect.signature(fn)
-        bound_args = dict(sig.bind(self, *args, **kwargs).arguments)
-        bound_args.pop("self")
-        if getattr(self, "_cfg", None) is None:
-            self._cfg = bound_args
-        return fn(self, *args, **kwargs)
-
-    return new_init
-
-
-class ComponentMeta(ABCMeta):
-    def __new__(mcs, name, bases, class_dict):
-        if "__init__" in class_dict:
-            wrapped = save_init_kwargs(class_dict["__init__"])
-            class_dict["__init__"] = wrapped
-            # wrapped.vd.model.__name__ = name
-        return super().__new__(mcs, name, bases, class_dict)
-
-    @property
-    def factory_name(cls):
-        return next(k for k, v in registry.factory.get_all().items() if cls == v)
-
-
-class Component(Generic[InT, OutT], metaclass=ComponentMeta):
+class Component(Generic[InT, OutT]):
     def __init__(
         self,
         scorer: Optional[Scorer[OutT]] = None,
@@ -127,38 +103,35 @@ class Component(Generic[InT, OutT], metaclass=ComponentMeta):
         self.needs_training = False
         self.initialized = False
 
-    #    @classmethod
-    #    def __get_validators__(cls):
-    #        # one or more validators may be yielded which will be called in the
-    #        # order to validate the input, each validator will receive as an input
-    #        # the value returned from the previous validator
-    #        yield cls.validate
-    #
-    #    @classmethod
-    #    def validate(cls, v):
-    #        if isinstance(v, dict):
-    #            return cls(**v)
-    #        if isinstance(v, cls):
-    #            return v
-    #        raise ValueError(f"Could not cast {cls.__name__} from {v}")
-
-    @property
-    def factory_name(self):
-        return self.__class__.factory_name
-
-    @property
-    def cfg(self):
-        return {"@factory": self.factory_name, **self._cfg}  # noqa
-
-    def reset_cache(self, cache=None):
+    def reset_cache(self, cache: Optional[CacheEnum] = None):
         pass
 
     def initialize(self, gold_data: Iterable[OutT]):
-        pass
+        """
+        Initialize the missing properties of the component, such as its vocabulary,
+        using the gold data.
+
+        Parameters
+        ----------
+        gold_data: Iterable[OutT]
+            Gold data to use for initialization
+        """
 
     @abstractmethod
     def __call__(self, doc: InT) -> OutT:
-        """Transformers the document"""
+        """
+        Processes a single document
+
+        Parameters
+        ----------
+        doc: InT
+            Document to process
+
+        Returns
+        -------
+        OutT
+            Processed document
+        """
 
     def batch_process(self, docs: Sequence[InT], refs=None) -> Sequence[OutT]:
         return [self(doc) for doc in docs]
@@ -173,7 +146,11 @@ class Component(Generic[InT, OutT], metaclass=ComponentMeta):
 
     def pipe(self, docs: Iterable[InT], batch_size=1) -> Iterable[OutT]:
         """
-        Apply the pipeline on a collection of documents
+        Applies the component on a collection of documents. It is recommended to use
+        the [`Pipeline.pipe`][edspdf.pipeline.Pipeline.pipe] method instead of this one
+        to apply a pipeline on a collection of documents, to benefit from the caching
+        of intermediate results and avoiding loading too many documents in memory at
+        once.
 
         Parameters
         ----------
@@ -181,9 +158,6 @@ class Component(Generic[InT, OutT], metaclass=ComponentMeta):
             Input docs
         batch_size: int
             Batch size to use when making batched to be process at once
-
-        Returns
-        -------
         """
         for batch in batchify(docs, batch_size=batch_size):
             self.reset_cache()
@@ -192,9 +166,6 @@ class Component(Generic[InT, OutT], metaclass=ComponentMeta):
 
 class ModuleMeta(ABCMeta):
     def __new__(meta, name, bases, class_dict):
-        if "__init__" in class_dict:
-            wrapped = save_init_kwargs(class_dict["__init__"])
-            class_dict["__init__"] = wrapped
         if "preprocess" in class_dict:
             class_dict["preprocess"] = cached_preprocess(class_dict["preprocess"])
         if "collate" in class_dict:
@@ -204,12 +175,15 @@ class ModuleMeta(ABCMeta):
 
         return super().__new__(meta, name, bases, class_dict)
 
-    @property
-    def factory_name(cls):
-        return next(k for k, v in registry.factory.get_all().items() if cls == v)
-
 
 class Module(torch.nn.Module, Generic[InT, BatchT], metaclass=ModuleMeta):
+    """
+    Base class for all EDS-PDF modules. This class is an extension of Pytorch's
+    `torch.nn.Module` class. It adds a few methods to handle preprocessing and collating
+    features, as well as caching intermediate results for components that share a common
+    subcomponent.
+    """
+
     IS_MODULE = True
 
     def __init__(self):
@@ -225,16 +199,28 @@ class Module(torch.nn.Module, Generic[InT, BatchT], metaclass=ModuleMeta):
         yield
         self.enable_cache(saved)
 
-    @property
-    def factory_name(self):
-        return self.__class__.factory_name
+    def initialize(self, gold_data: Iterable[InT], **kwargs):
+        """
+        Initialize the missing properties of the module, such as its vocabulary,
+        using the gold data and the provided keyword arguments.
 
-    @property
-    def cfg(self):
-        return {"@factory": self.factory_name, **self._cfg}  # noqa
-
-    def initialize(self, gold_data: Iterable[InT]):
-        pass
+        Parameters
+        ----------
+        gold_data: Iterable[InT]
+            Gold data to use for initialization
+        kwargs: Any
+            Additional keyword arguments to use for initialization
+        """
+        for name, value in kwargs.items():
+            if value is None:
+                continue
+            current_value = getattr(self, name)
+            if current_value is not None and current_value != value:
+                raise ValueError(
+                    "Cannot initialize module with different values for "
+                    "attribute '{}': {} != {}".format(name, current_value, value)
+                )
+            setattr(self, name, value)
 
     def enable_cache(self, do_cache):
         saved = self._do_cache
@@ -250,9 +236,7 @@ class Module(torch.nn.Module, Generic[InT, BatchT], metaclass=ModuleMeta):
     def device(self):
         return next(iter(self.parameters())).device
 
-    def reset_cache(self, cache=None):
-        # print("Resetting", cache, "in", self.__class__.__name__)
-
+    def reset_cache(self, cache: Optional[CacheEnum] = None):
         def clear(module):
             try:
                 assert cache is None or cache in (
@@ -271,8 +255,6 @@ class Module(torch.nn.Module, Generic[InT, BatchT], metaclass=ModuleMeta):
 
         self.apply(clear)
 
-    module_forward = torch.nn.Module.__call__
-
     def preprocess(self, doc: InT, supervision: bool = False) -> Dict[str, Any]:
         """
         Parameters
@@ -290,18 +272,27 @@ class Module(torch.nn.Module, Generic[InT, BatchT], metaclass=ModuleMeta):
         """Collate operation : should return some tensors"""
         return {}
 
-    def forward(self, batch: BatchT, supervision=False) -> Dict[str, Any]:
+    def forward(self, *args, **kwargs) -> Dict[str, Any]:
         """Forward pass of the torch module"""
-        return batch
+        raise NotImplementedError()
+
+    def module_forward(self, *args, **kwargs):
+        """
+        This is a wrapper around `torch.nn.Module.__call__` to avoid conflict
+        with the [`Component.__call__`][edspdf.component.Component.__call__]
+         method.
+        """
+        return torch.nn.Module.__call__(self, *args, **kwargs)
 
 
-class TrainableComponentMeta(ComponentMeta, ModuleMeta):
-    pass
+class TrainableComponent(Module[InT, BatchT], Component[InT, OutT]):
+    """
+    A TrainableComponent is a Component that can be trained and inherits from both
+    `Module` and `Component`. You can therefore use it either as a torch module inside
+    a more complex neural network, or as a standalone component in a
+    [Pipeline][edspdf.pipeline.Pipeline].
+    """
 
-
-class TrainableComponent(
-    Module[InT, BatchT], Component[InT, OutT], metaclass=TrainableComponentMeta
-):
     def __init__(self):
         Module.__init__(self)
         Component.__init__(self)
@@ -328,6 +319,9 @@ class TrainableComponent(
             res = self.forward(inputs)
             docs = self.postprocess(docs, res)
             return docs
+
+    def forward(self, batch: BatchT, supervision=False) -> Dict[str, Any]:
+        return batch
 
     def postprocess(self, docs: Sequence[InT], batch: Dict[str, Any]) -> Sequence[OutT]:
         return docs
--- edspdf/pipeline.py
+++ edspdf/pipeline.py
@@ -2,6 +2,7 @@ import copy
 import time
 from collections import defaultdict
 from contextlib import contextmanager
+from functools import partial
 from pathlib import Path
 from textwrap import indent
 from typing import (
@@ -19,12 +20,13 @@ from typing import (
 import torch
 from tqdm import tqdm
 
-from .component import Component, TrainableComponent
+from .component import CacheEnum, Component, TrainableComponent
 from .config import Config, validate_arguments
 from .registry import registry
-from .utils.collections import batch_compress_dict, batchify, decompress_dict
+from .utils.collections import batch_compress_dict, batchify, decompress_dict, multi_tee
 
-T = TypeVar("T")
+InputT = TypeVar("InputT")
+OutputT = TypeVar("OutputT")
 
 
 class ComponentsMap(dict):
@@ -42,6 +44,21 @@ class ComponentsMap(dict):
 
 @validate_arguments
 class Pipeline:
+    """
+    The Pipeline is the core object of EDS-PDF. It is responsible for the
+    orchestration of the components and processing PDF documents end-to-end.
+
+    A pipeline is usually created empty and then populated with components via the
+    `add_pipe` method. Here is an example :
+
+    ```python
+    pipeline = Pipeline()
+    pipeline.add_pipe("pdfminer-extractor")
+    pipeline.add_pipe("mask-classifier")
+    pipeline.add_pipe("simple-aggregator")
+    ```
+    """
+
     def __init__(
         self,
         components: Optional[List[str]] = None,
@@ -49,13 +66,21 @@ class Pipeline:
         batch_size: int = 4,
     ):
         """
-        Pipeline object
+        Initializes the pipeline. The pipeline is empty by default and can be
+        populated with components via the `add_pipe` method.
 
         Parameters
         ----------
-        components
-        components_config
-        batch_size
+        components: Optional[List[str]]
+            List of component names
+        components_config: Optional[Dict[str, Component]]
+            Dictionary of component configurations. The keys of the dictionary must
+            match the component names. The values are the component configurations,
+            which can contain unresolved configuration for nested components or
+            instances of components.
+        batch_size: int
+            The default number of documents to process in parallel when running the
+            pipeline
         """
         super().__init__()
         if components is None:
@@ -71,38 +96,33 @@ class Pipeline:
         self.batch_size = batch_size
         self.meta = {}
 
-    @property
-    def trainable_components(self) -> List[TrainableComponent]:
-        return [
-            c
-            for c in self.components.values()
-            if isinstance(c, TrainableComponent) and c.needs_training
-        ]
-
-    def __iter__(self):
-        return iter(self.components.values())
-
-    def __len__(self):
-        return len(self.components)
-
     def add_pipe(self, factory_name: Union[str, Callable], name=None, config=None):
         """
-        Add a component to the pipeline
+        Adds a component to the pipeline. The component can be either a factory name or
+        an instantiated component. If a factory name is provided, the component will be
+        instantiated with the class from the registry matching the factory name and
+        using the provided config as arguments.
 
         Parameters
         ----------
-        factory_name
-        name
-        config
+        factory_name: Union[str, Callable]
+            Either a factory name or an instantiated component
+        name: str
+            Name of the component
+        config: Dict
+            Configuration of the component. The configuration can contain unresolved
+            configuration for nested components such as
+            `{"@factory_name": "my-sub-component", ...}`
 
         Returns
         -------
-
+        Component
+            The added component
         """
         if isinstance(factory_name, str):
             if config is None:
                 config = {}
-            config = Config(config).resolve()
+            # config = Config(config).resolve()
             cls = registry.factory.get(factory_name)
             component = cls(**config)
         elif hasattr(factory_name, "__call__") or hasattr(factory_name, "process"):
@@ -111,7 +131,11 @@ class Pipeline:
                     "Cannot provide both an instantiated component and its config"
                 )
             component = factory_name
-            factory_name = getattr(component, "factory_name", None)
+            factory_name = next(
+                k
+                for k, v in registry.factory.get_all().items()
+                if component.__class__ == v
+            )
         else:
             raise TypeError(
                 "`add_pipe` first argument must either be a factory name "
@@ -134,17 +158,15 @@ class Pipeline:
 
         return component
 
-    def reset_cache(self, cache=None):
+    def reset_cache(self, cache: Optional[CacheEnum] = None):
         """
         Reset the caches of the components in this pipeline
 
         Parameters
         ----------
-        cache
-
-        Returns
-        -------
-
+        cache: Optional[CacheEnum]
+            The cache to reset (either `preprocess`, `collate` or `forward`)
+            If None, all caches are reset
         """
         for component in self.components.values():
             try:
@@ -152,98 +174,54 @@ class Pipeline:
             except AttributeError:
                 pass
 
-    def __call__(self, doc):
+    def __call__(self, doc: InputT) -> OutputT:
         """
         Applies the pipeline on a sample
 
         Parameters
         ----------
-        doc
+        doc: InputT
+            The document to process
 
         Returns
         -------
-
+        OutputT
         """
         self.reset_cache()
         for name, component in self.components.items():
             doc = component(doc)
         return doc
 
-    def train(self, mode=True):
-        """
-        Enables training on pytorch modules
-
-        Parameters
-        ----------
-        mode
-
-        Returns
-        -------
-
-        """
-        for component in self.components.values():
-            if hasattr(component, "train"):
-                component.train(mode)
-
-    @property
-    def cfg(self):
-        return Config(
-            components=list(self.components.keys()),
-            components_config=Config(**self.components, __path__=("components",)),
-        )
-
-    @contextmanager
-    def no_cache(self):
-        """
-        Disable caching
-
-        Returns
-        -------
-
-        """
-        saved = []
-        for component in self.components.values():
-            if isinstance(component, TrainableComponent):
-                saved.append((component, component.enable_cache(False)))
-        yield
-        for component, do_cache in saved:
-            component.enable_cache(do_cache)
-
-    def pipe(self, docs: Iterable) -> Iterable:
+    def pipe(self, docs: Iterable[InputT]) -> Iterable:
         """
         Apply the pipeline on a collection of documents
 
         Parameters
         ----------
-        docs
+        docs: Iterable[InputT]
+            The documents to process
 
         Returns
         -------
-
+        Iterable
+            An iterable collection of processed documents
         """
-        # Pourquoi pre-batch et pas enchainement d'iterateurs ?
-        # Car:
-        # - si iterateurs, tailles de batch par comp différentes (comp1: 16, comp2: 32)
-        # - or, pour cacher efficacement il faut égalité entre batch
-        # - tbc
-
         for batch in batchify(docs, batch_size=self.batch_size):
             self.reset_cache()
             for component in self.components.values():
                 batch = component.batch_process(batch)
             yield from batch
 
-    def initialize(self, data: Iterable[T]):
+    def initialize(self, data: Iterable[InputT]):
         """
         Initialize the components of the pipeline
-        Each component must be initialized before
-        the next components are run. Since a component might need
-        the full training data to be initialized, all data may be
-        fed to the component, making it impossible to enable batch caching.
+        Each component must be initialized before the next components are run.
+        Since a component might need the full training data to be initialized, all
+        data may be fed to the component, making it impossible to enable batch caching.
 
-        Therefore, we disable cache during the entire operation, so
-        heavy computation (such as embeddings) that is usually shared will
-        be repeated for each initialized component.
+        Therefore, we disable cache during the entire operation, so heavy computation
+        (such as embeddings) that is usually shared will be repeated for each
+        initialized component.
 
         Parameters
         ----------
@@ -252,26 +230,34 @@ class Pipeline:
         """
         # Component initialization
         print("Initializing components")
+        data = multi_tee(data)
 
-        for name, component in self.components.items():
-            if not component.initialized:
-                component.initialize(data)
-                print(f"Component {repr(name)} initialized")
+        with self.no_cache():
+            for name, component in self.components.items():
+                if not component.initialized:
+                    component.initialize(data)
+                    print(f"Component {repr(name)} initialized")
 
-    def score(self, data: Sequence[T]):
+    def score(self, docs: Sequence[InputT]):
         """
-        Score a pipeline against a supervised dataset
+        Scores a pipeline against a sequence of annotated documents
+
         Parameters
         ----------
-        data
+        docs: Sequence[InputT]
+            The documents to score
 
         Returns
         -------
-
+        Dict[str, Any]
+            A dictionary containing the metrics of the pipeline, as well as the speed of
+            the pipeline. Each component that has a scorer will also be scored and its
+            metrics will be included in the returned dictionary under a key named after
+            each component.
         """
         self.train(False)
-        inputs: Sequence[T] = copy.deepcopy(data)
-        golds: Iterable[Dict[str, T]] = data
+        inputs: Sequence[InputT] = copy.deepcopy(docs)
+        golds: Iterable[Dict[str, InputT]] = docs
 
         scored_components = {}
 
@@ -300,25 +286,73 @@ class Pipeline:
             metrics[name] = component.score(list(zip(preds[name], golds)))
         return metrics
 
-    def preprocess(self, doc: T, supervision: bool = False):
+    def preprocess(self, doc: InputT, supervision: bool = False):
+        """
+        Runs the preprocessing methods of each component in the pipeline
+        on a document and returns a dictionary containing the results, with the
+        component names as keys.
+
+        Parameters
+        ----------
+        doc: InputT
+            The document to preprocess
+        supervision: bool
+            Whether to include supervision information in the preprocessing
+
+        Returns
+        -------
+        Dict[str, Any]
+        """
         prep = {}
         for name, component in self.components.items():
             if isinstance(component, TrainableComponent):
                 prep[name] = component.preprocess(doc, supervision=supervision)
         return prep
 
-    def preprocess_many(self, docs: Iterable[T], compress=True, supervision=True):
-        preprocessed = (self.preprocess(doc, supervision=supervision) for doc in docs)
+    def preprocess_many(self, docs: Iterable[InputT], compress=True, supervision=True):
+        """
+        Runs the preprocessing methods of each component in the pipeline on
+        a collection of documents and returns an iterable of dictionaries containing
+        the results, with the component names as keys.
+
+        Parameters
+        ----------
+        docs: Iterable[InputT]
+        compress: bool
+            Whether to deduplicate identical preprocessing outputs of the results
+            if multiple documents share identical subcomponents. This step is required
+            to enable the cache mechanism when training or running the pipeline over a
+            tabular datasets such as pyarrow tables that do not store referential
+            equality information.
+        supervision: bool
+            Whether to include supervision information in the preprocessing
+
+        Returns
+        -------
+        Iterable[OutputT]
+        """
+        preprocessed = map(partial(self.preprocess, supervision=supervision), docs)
         if compress:
             return batch_compress_dict(preprocessed)
         return preprocessed
 
-    def parameters(self):
-        for component in self.components.values():
-            if isinstance(component, torch.nn.Module):
-                yield from component.parameters()
+    def collate(self, batch: Dict[str, Any], device: Optional[torch.device] = None):
+        """
+        Collates a batch of preprocessed samples into a single (maybe nested)
+        dictionary of tensors by calling the collate method of each component.
 
-    def collate(self, batch, device: Optional[torch.device] = None):
+        Parameters
+        ----------
+        batch: Dict[str, Any]
+            The batch of preprocessed samples
+        device: Optional[torch.device]
+            The device to move the tensors to before returning them
+
+        Returns
+        -------
+        Dict[str, Any]
+            The collated batch
+        """
         batch = decompress_dict(batch)
         if device is None:
             device = next(p.device for p in self.parameters())
@@ -328,6 +362,50 @@ class Pipeline:
                 component_inputs = batch[name]
                 batch[name] = component.collate(component_inputs, device)
         return batch
+
+    def train(self, mode=True):
+        """
+        Enables training mode on pytorch modules
+
+        Parameters
+        ----------
+        mode: bool
+            Whether to enable training or not
+        """
+        for component in self.components.values():
+            if hasattr(component, "train"):
+                component.train(mode)
+
+    @property
+    def cfg(self):
+        """Returns the initial configuration of the pipeline"""
+        return Config(
+            components=list(self.components.keys()),
+            components_config=Config(**self.components, __path__=("components",)),
+        ).serialize()
+
+    @contextmanager
+    def no_cache(self):
+        """Disable caching for all (trainable) components in the pipeline"""
+        saved = []
+        for component in self.components.values():
+            if isinstance(component, TrainableComponent):
+                saved.append((component, component.enable_cache(False)))
+        yield
+        for component, do_cache in saved:
+            component.enable_cache(do_cache)
+
+    def parameters(self):
+        """Returns an iterator over the Pytorch parameters of the components in the
+        pipeline"""
+        seen = set()
+        for component in self.components.values():
+            if isinstance(component, torch.nn.Module):
+                for param in component.parameters():
+                    if param in seen:
+                        continue
+                    seen.add(param)
+                    yield param
 
     def __repr__(self):
         return "Pipeline({})".format(
@@ -340,6 +418,23 @@ class Pipeline:
             if len(self.components)
             else ""
         )
+
+    @property
+    def trainable_components(self) -> List[TrainableComponent]:
+        """Returns the list of trainable components in the pipeline."""
+        return [
+            c
+            for c in self.components.values()
+            if isinstance(c, TrainableComponent) and c.needs_training
+        ]
+
+    def __iter__(self):
+        """Returns an iterator over the components in the pipeline."""
+        return iter(self.components.values())
+
+    def __len__(self):
+        """Returns the number of components in the pipeline."""
+        return len(self.components)
 
 
 def load(config: Union[Path, str, Config]):
