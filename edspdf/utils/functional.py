a1589e69ad794b13b0746ed1a05308337ec3a661
Perceval Wajsbürt <perceval.wajsburt-ext@aphp.fr>
2023-01-24 17:02:01 +0000

    feat: update utils

--- edspdf/utils/functional.py
+++
@@ -1,8 +0,0 @@
-class compose:
-    def __init__(self, *functions):
-        self.functions = functions
-
-    def __call__(self, arg):
-        for fn in self.functions:
-            arg = fn(arg)
-        return arg
